efc5a5a396480649eb00b34a6887b795435586ce
Devon Quest <devonquest@mmc.school.nz>
2019-07-24 13:18:12 +1200

    added timer and new code

---
+++ Kermit/FrmKermit.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Kermit
+{
+    partial class FrmKermit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PnlGame = new System.Windows.Forms.Panel();
+            this.TmrPlanet = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // PnlGame
+            // 
+            this.PnlGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(148)))), ((int)(((byte)(73)))));
+            this.PnlGame.Location = new System.Drawing.Point(32, 33);
+            this.PnlGame.Name = "PnlGame";
+            this.PnlGame.Size = new System.Drawing.Size(500, 400);
+            this.PnlGame.TabIndex = 0;
+            this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlGame_Paint);
+            // 
+            // TmrPlanet
+            // 
+            this.TmrPlanet.Enabled = true;
+            this.TmrPlanet.Tick += new System.EventHandler(this.TmrPlanet_Tick);
+            // 
+            // FrmKermit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.PnlGame);
+            this.Name = "FrmKermit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kermit";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlGame;
+        private System.Windows.Forms.Timer TmrPlanet;
+    }
+}
+
---
+++ Kermit/FrmKermit.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kermit
+{
+    public partial class FrmKermit : Form
+    {
+        Graphics g; // declare the graphics object
+        int x = 20, y = 20;// starting position of planet
+        Random speed = new Random();
+        int speed1, speed2, speed3, speed4, speed5, speed6, speed7;
+        //Declare the rectangles to display the spaceship and planets in
+        Rectangle area, area1, area2, area3, area4, area5, area6, area7;
+
+        private void TmrPlanet_Tick(object sender, EventArgs e)
+        {
+            area1.Y += speed1;// move the area(planet) down the panel
+            if (area1.Y > PnlGame.Height)
+            {
+                area1.Y = 20;
+            }
+            area2.Y += speed2;// move the area(planet) down the panel
+            PnlGame.Invalidate();//makes the paint event fire to redraw the panel
+            if (area2.Y > PnlGame.Height)
+            {
+                area2.Y = 20;
+            }
+            area3.Y += speed3;// move the area(planet) down the panel
+            if (area3.Y > PnlGame.Height)
+            {
+                area3.Y = 20;
+            }
+            area4.Y += speed4;// move the area(planet) down the panel
+            if (area4.Y > PnlGame.Height)
+            {
+                area4.Y = 20;
+            }
+            area5.Y += speed5;// move the area(planet) down the panel
+            if (area5.Y > PnlGame.Height)
+            {
+                area5.Y = 20;
+            }
+            area6.Y += speed6;// move the area(planet) down the panel
+            if (area6.Y > PnlGame.Height)
+            {
+                area6.Y = 20;
+            }
+            area7.Y += speed7;// move the area(planet) down the panel
+            if (area7.Y > PnlGame.Height)
+            {
+                area7.Y = 20;
+            }
+
+        }
+
+        int x2 = 50, y2 = 290; //starting position of spaceship
+        //Load our two images from the bin\debug folder
+        Image spaceship = Image.FromFile(Application.StartupPath + @"\alien1.png");
+        Image planet1 = Image.FromFile(Application.StartupPath + @"\planet1.png");
+
+
+        private void pnlGame_Paint(object sender, PaintEventArgs e)
+        {
+            //get the methods from the graphic's class to paint on the panel
+            g = e.Graphics;
+            //use the DrawImage method to draw the spaceship on the panel
+            g.DrawImage(spaceship, area);
+            //use the DrawImage method to draw the planet on the panel
+            g.DrawImage(planet1, area1);
+            g.DrawImage(planet1, area2);
+            g.DrawImage(planet1, area3);
+            g.DrawImage(planet1, area4);
+            g.DrawImage(planet1, area5);
+            g.DrawImage(planet1, area6);
+            g.DrawImage(planet1, area7);
+
+        }
+
+        public FrmKermit()
+        {
+            InitializeComponent();
+            area = new Rectangle(x2, y2, 30, 30);//spaceship's rectangle	
+            area1 = new Rectangle(x, y, 40, 40); //planet1's rectangle
+            area2 = new Rectangle(x + 70, y, 40, 40);// planet2's rectangle
+            area3 = new Rectangle(x + 140, y, 40, 40);// planet3's rectangle
+            area4 = new Rectangle(x + 210, y, 40, 40);// planet4's rectangle
+            area5 = new Rectangle(x + 280, y, 40, 40);// planet5's rectangle
+            area6 = new Rectangle(x + 350, y, 40, 40);// planet6's rectangle
+            area7 = new Rectangle(x + 420, y, 40, 40);// planet7's rectangle
+            speed2 = speed.Next(5, 10);//planet1's speed will be between 5 and 10
+            speed3 = speed.Next(5, 10);//planet1's speed will be between 5 and 10
+            speed4 = speed.Next(5, 10);//planet1's speed will be between 5 and 10
+            speed5 = speed.Next(5, 10);//planet1's speed will be between 5 and 10
+            speed6 = speed.Next(5, 10);//planet1's speed will be between 5 and 10
+            speed7 = speed.Next(5, 10);//planet1's speed will be between 5 and 10
+
+
+            speed1 = speed.Next(5, 10);//planet1's speed will be between 5 and 10
+
+        }
+    }
+}
