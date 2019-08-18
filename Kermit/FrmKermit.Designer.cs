b41813b5ab2fd0363a94f6a8ce2c1370631b101e
Devon Quest <devonquest@mmc.school.nz>
2019-08-19 09:02:39 +1200

    changed spaceships back to planets

--- Kermit/FrmKermit.Designer.cs
+++ Kermit/FrmKermit.Designer.cs
@@ -105,7 +105,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(207, 41);
+            this.label5.Location = new System.Drawing.Point(200, 41);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(180, 38);
             this.label5.TabIndex = 4;
